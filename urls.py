cb6357f41b727b9602fe657b70b3b02eef50735b
Zach McCormick <zachary.tyler.mccormick@gmail.com>
2023-07-18 20:19:40 -0400

    feature(tracing): add django-decorator-include and add admin and auth services

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from functools import partial
 
+from decorator_include import decorator_include
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,12 +8,14 @@ import app.inventory.views
 import app.payment.views
 from lib.tracing import for_service
 
+admin_app = partial(for_service, 'admin')
+auth_app = partial(for_service, 'auth')
 inventory_app = partial(for_service, 'inventory')
 payment_app = partial(for_service, 'payment')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('admin/', decorator_include(admin_app(), admin.site.urls)),
+    path('accounts/', decorator_include(auth_app(), 'allauth.urls')),
 
     path('', inventory_app()(app.inventory.views.LeftoversAllView.as_view()), name='leftovers-all'),
     path('leftovers/mine', inventory_app()(app.inventory.views.LeftoversMineView.as_view()), name='leftovers-mine'),
